8544cf1c6b48a1902a4b1a6f611ca3c5c25bc469
henrique11varela <henrique11varela@gmail.com>
2023-01-01 20:54:52 +0000

    added tuner and modded ssd1306

--- SSD1306_tester/SSD1306_tester.ino
+++ SSD1306_tester/SSD1306_tester.ino
@@ -3,18 +3,49 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
+
 #define SCREEN_WIDTH 128
-#define SCREEN_HEIGT 64
+#define SCREEN_HEIGHT 64
 #define OLED_RESET -1
 #define SCREEN_ADDRESS 0x3C
+
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
+int x = 63, y = 31;
+bool xIncrement = true, yIncrement = true;
+int r = 4;
 
 void setup() {
-  Serial.begin(9600);
-
+  display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS); //starts screen
+  display.clearDisplay();
+  display.display();
 }
 
 void loop() {
+  if (x > SCREEN_WIDTH - r)
+    xIncrement = false;
+  else if (x < 0 + r)
+    xIncrement = true;
 
+  if (y > SCREEN_HEIGHT - r)
+    yIncrement = false;
+  else if (y < 0 + r)
+    yIncrement = true;
+
+  if (xIncrement)
+    x++;
+  else
+    x--;
+
+  if (yIncrement)
+    y++;
+  else
+    y--;
+
+  display.clearDisplay();
+  display.drawCircle(x, y, r, WHITE);
+  display.display();
 }
+
+//clears display buffer
+//display.clearDisplay();
